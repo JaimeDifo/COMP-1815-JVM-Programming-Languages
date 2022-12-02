05b6c7051004d98081c4ed42b3d99a25c4a8bf64
Doodpool <47264853+Egeguzelgonul@users.noreply.github.com>
2022-12-02 11:54:10 +0000

    Search Update
    Added Search Feature

--- Coursework/src/main/kotlin/GUI.java
+++ Coursework/src/main/kotlin/GUI.java
@@ -1,6 +1,7 @@
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.*;
@@ -9,20 +10,24 @@ import java.util.List;
 
 public class GUI {
     private JTextField databaseLocationTextfield;
-    DefaultListModel<String> model = new DefaultListModel<>();
-    public JList<String> MasterList;
     private JPanel GUI_Window;
-    private JButton showRecordsButton;
-    private JLabel recordLabel;
     private JButton loadButton;
     private JButton deleteButton;
     private JButton addButton;
     private JButton editButton;
+    private JTable MasterTable;
+    private JComboBox comboBox1;
+    private JButton sortButton;
+    private JTextField searchField;
+    private JButton searchButton;
+    private JComboBox searchCategory;
     static Connection con;
     static Statement stmt;
     public static int index;
     public static boolean isEditing = false;
     List<Book> bookList =  new ArrayList<>();
+    String searchedItem = "";
+    Integer searchedColumn = 1;
 
     public void Connect()
     {
@@ -30,46 +35,119 @@ public class GUI {
             Class.forName("org.sqlite.JDBC");
             con = DriverManager.getConnection("jdbc:sqlite:" + databaseLocationTextfield.getText());
             System.out.println("Success");
+            showRecords();
         }
         catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
-
+    public void bubbleSort(){
+//        for (int i=0;i<bookList.size()-1;++i){
+//
+//            for(int j=0;j<bookList.size()-i-1; ++j){
+//                String title_1 = bookList.get(j+1).getTitle().toLowerCase();
+//                String title_2 = bookList.get(j).getTitle().toLowerCase();
+//                if(title_1.compareTo(title_2)<0){
+//                    Book swap = bookList.get(j);
+//                    bookList.set(j, bookList.get(j+1));
+//                    bookList.set(j+1, swap);
+//                }
+//            }
+//        }
+        main test = new main();
+        test.bubbleSort(bookList);
+    }
     public void showRecords()
     {
         try {
+            bookList.clear();
             stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery( "SELECT * FROM Books;" );
 
-            model.removeAllElements();
+            String column[]={"id", "title", "author", "publisher", "subject", "year"};
+            String data[][]={};
+            DefaultTableModel tableModel = new DefaultTableModel(data, column);
 
             while(rs.next()){
                 int id = rs.getInt("ID");
-                String name = rs.getString("Title");
+                String title = rs.getString("Title");
                 String author = rs.getString("Author");
                 String publisher = rs.getString("Publisher");
                 String subject = rs.getString("Subject");
                 int year = rs.getInt("Year");
 
-                Book newBook = new Book(id,name,author,publisher,subject,year);
+                Object[] newRow = new Object[]{
+                        rs.getInt("ID"),
+                        rs.getString("Title"),
+                        rs.getString("Author"),
+                        rs.getString("Publisher"),
+                        rs.getString("Subject"),
+                        rs.getInt("Year")};
+
+                if (!searchedItem.equals("")){
+                    if (searchedItem.equals(newRow[searchedColumn].toString())){
+                        tableModel.addRow(newRow);
+                    }
+                }
+                else {
+                    tableModel.addRow(newRow);
+                }
+
+                Book newBook = new Book(id,title,author,publisher,subject,year);
                 bookList.add(newBook);
-                model.addElement(newBook.toString());
-                MasterList.setModel(model);
             }
+            MasterTable.setModel(tableModel);
+            searchedItem = "";
         }
         catch (SQLException e1)
         {
             e1.printStackTrace();
         }
     }
-    public GUI() {
-        showRecordsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                showRecords();
+    public void saveAllRecord(){
+        //DELETE FROM Customers;
+        try{
+            // Clearing all rows from table
+            stmt.executeUpdate("DELETE FROM Books");
+            // Make auto increment restart from 1
+            stmt.executeUpdate("UPDATE sqlite_sequence SET seq=0 WHERE name='Books'");
+            for (Book book : bookList) {
+                int rs = GUI.stmt.executeUpdate( "INSERT INTO Books (Title, Author, Publisher, Subject, Year)" +
+                        "VALUES ('" + book.getTitle() + "', '" + book.getAuthor() + "', '" + book.getPublisher() + "', '" + book.getSubject() + "', '" + book.getYear() + "')" );
             }
-        });
+
+        }
+        catch (SQLException e1)
+        {
+            e1.printStackTrace();
+        }
+    }
+
+    public void search(){
+        searchedItem = searchField.getText();
+        searchedColumn = searchCategory.getSelectedIndex() + 1;
+        //System.out.println(searchedItem);
+        showRecords();
+    }
+
+    //PARSE THE TABLE MODEL AS AN ARRAY
+    public Object[][] getTableData (JTable table) {
+        DefaultTableModel dtm = (DefaultTableModel) table.getModel();
+        int nRow = dtm.getRowCount(), nCol = dtm.getColumnCount();
+        Object[][] tableData = new Object[nRow][nCol];
+        for (int i = 0 ; i < nRow ; i++)
+            for (int j = 0 ; j < nCol ; j++)
+                tableData[i][j] = dtm.getValueAt(i,j);
+        return tableData;
+    }
+    public GUI() {
+//        showRecordsButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                showRecords();
+//            }
+//        });
+
         loadButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -85,17 +163,19 @@ public class GUI {
                 addRecordPopup.setLocationRelativeTo(null);
                 addRecordPopup.pack();
                 addRecordPopup.show();
+                showRecords();
             }
         });
         editButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 isEditing = true;
-                
+
                 addRecordPopup editPopup = new addRecordPopup();
                 editPopup.setLocationRelativeTo(null);
                 editPopup.pack();
                 editPopup.show();
+                showRecords();
             }
         });
         deleteButton.addActionListener(new ActionListener() {
@@ -105,12 +185,31 @@ public class GUI {
                 deletionPopup.setLocationRelativeTo(null);
                 deletionPopup.pack();
                 deletionPopup.show();
+                showRecords();
             }
         });
-        MasterList.addListSelectionListener(new ListSelectionListener() {
+        MasterTable.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
             @Override
-            public void valueChanged(ListSelectionEvent e) {
-                index = MasterList.getSelectedIndex() + 1;
+            public void valueChanged(ListSelectionEvent event) {
+                if(event.getValueIsAdjusting()) {
+                    if(MasterTable.getSelectedRow() > 0) {
+                        index = Integer.parseInt((MasterTable.getValueAt(MasterTable.getSelectedRow(), 0)).toString());
+                        System.out.println(index);
+                    }
+                }
+            }
+        });
+        sortButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                bubbleSort();
+                saveAllRecord();
+            }
+        });
+        searchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                search();
             }
         });
     }
@@ -122,6 +221,7 @@ public class GUI {
         frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
+
     }
 }
 
