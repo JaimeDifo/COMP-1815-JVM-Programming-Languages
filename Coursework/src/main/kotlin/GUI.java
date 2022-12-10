50bac01931e3e45cbe119be0a43efcc0281542b8
Doodpool <47264853+Egeguzelgonul@users.noreply.github.com>
2022-12-10 15:35:13 +0000

    GUI & Main commented

--- Coursework/src/main/kotlin/GUI.java
+++ Coursework/src/main/kotlin/GUI.java
@@ -3,7 +3,6 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.sql.Statement;
 import java.util.function.Supplier;
 
 public class GUI {
@@ -22,21 +21,25 @@ public class GUI {
     public JComboBox dataCategory;
     private JPanel displayTime;
     private JLabel ticksLabel;
-    static Statement stmt;
     static Integer index;
     public static boolean isEditing = false;
     Supplier<Integer> dataType = () -> {
-        return dataCategory.getSelectedIndex();
+        return dataCategory.getSelectedIndex(); // A lambda function which will return the index number of the current data type
     };
 
     public GUI() {
 
+        //The Load Database button
         loadButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //Once the button is pressed, sql connection to the database will be established
                 Main.Connect(databaseLocationTextfield.getText());
+                // The records to be displayed on the Table
                 Main.showRecords(MasterTable);
+                // The combo box initially contains "Not loaded" which is changed to the following once the database is loaded
                 String[] fields = {"Books", "Authors", "Publishers"};
+                // This is to prevent the user from switching tables whenever the database is not loaded which would result in an exception
                 DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>(fields);
                 dataCategory.setModel(model);
             }
@@ -44,8 +47,10 @@ public class GUI {
         addButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Since both Add and Edit features utilise the same dialogue box, there's a boolean indicating the current purpose of the dialogue box
                 isEditing = false;
 
+                // A dialogue box of different number of fields will initialise based on the datatype, since the number of fields tend to differ
                 if (dataType.get() == 0){
                     addRecordPopup addRecordPopup = new addRecordPopup();
                     addRecordPopup.setLocationRelativeTo(null);
@@ -64,16 +69,20 @@ public class GUI {
                     addRecordPopup.pack();
                     addRecordPopup.show();
                 }
+                // The records shown on the table refreshed to include the potential changes
                 Main.showRecords(MasterTable);;
             }
         });
         editButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // The Edit feature requires a row to be selected to function
                 if (!MasterTable.getSelectionModel().isSelectionEmpty())
                 {
+                    // The boolean variable is set to true, indicating that the dialogue box should utilise functions specific to editing
                     isEditing = true;
 
+                    // The data type check has been implemented similar to the Add feature
                     if(dataType.get() == 0){
                         addRecordPopup editPopup = new addRecordPopup();
                         editPopup.setLocationRelativeTo(null);
@@ -92,8 +101,10 @@ public class GUI {
                         editPopup.pack();
                         editPopup.show();
                     }
+                    // The records shown on the table refreshed to include the potential changes
                     Main.showRecords(MasterTable);
                 }
+                // In case selection has not been made, an error box will appear instead
                 else {
                     errorBox errorBox = new errorBox();
                     errorBox.setLocationRelativeTo(null);
@@ -105,15 +116,20 @@ public class GUI {
         deleteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // A row on the table has to be selected for Delete feature to function
                 if (!MasterTable.getSelectionModel().isSelectionEmpty())
                 {
+                    // This feature does not utilise datatype check
+                    // The following dialogue box will appear asking the user to confirm the deletion
                     deletionPopup deletionPopup = new deletionPopup();
                     deletionPopup.delTargetTable = (String) dataCategory.getSelectedItem();
                     deletionPopup.setLocationRelativeTo(null);
                     deletionPopup.pack();
                     deletionPopup.show();
+                    // The records shown on the table refreshed to include the potential changes
                     Main.showRecords(MasterTable);;
                 }
+                // An error box will appear unless a row has been selected
                 else
                 {
                     errorBox errorBox = new errorBox();
@@ -123,50 +139,62 @@ public class GUI {
                 }
             }
         });
+        // A selection listener for the Table, this section triggers whenever a selection has been made
         MasterTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent event) {
                 if (event.getValueIsAdjusting()) {
+                    // Since the selection index cannot be outside the bounds, the following check prevents further update unless the value is valid and non-negative
                     if (MasterTable.getSelectedRow() >= 0) {
+                        // The following integer is to store the ID number of the currently selected item
                         index = Integer.parseInt((MasterTable.getValueAt(MasterTable.getSelectedRow(), 0)).toString());
                     }
                 }
             }
         });
+        // The sort button
         sortButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // The type of the sorting algorithm is checked, and an appropriate method is called
+                // The table also gets refreshed after the algorithm has run
                 if ("Bubble sort" == sortCategory.getSelectedItem()) {
                     Main.bubbleSort();
                     Main.saveAllRecord();
                     Main.showRecords(MasterTable);
-//                    System.out.println(sortCategory.getSelectedItem());
                 }
                 if ("Merge sort" == sortCategory.getSelectedItem()) {
                     Main.mergeSort();
                     Main.saveAllRecord();
                     Main.showRecords(MasterTable);
-//                    System.out.println(sortCategory.getSelectedItem());
                 }
                 if ("Radix sort" == sortCategory.getSelectedItem()) {
-//                    System.out.println(sortCategory.getSelectedItem());
                     Main.radixSort();
                     Main.saveAllRecord();
                     Main.showRecords(MasterTable);
                 }
+                // The time it took to complete the sorting is displayed on the label
                 ticksLabel.setText(Main.Ticks);
             }
         });
+        // The search button
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // The searched item will be determined based on the input given on the search field
                 Main.searchedItem = searchField.getText();
+                // The records which match the searched item will be displayed on the table
                 Main.showRecords(MasterTable);
             }
         });
+        // Combo box displaying the data types (books, authors, publishers)
+        // It initially has only one item, "Not Loaded", to prevent the user from interacting with it
+        // Considering that the interacting with it requires the database to be loaded
+        // Otherwise, there could have been an error
         dataCategory.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // Based on the current datatype, column labels for the table will change accordingly
                 Main.dataType = dataCategory.getSelectedIndex();
                 if (Main.dataType == 0){
                     String[] fields = {"Title", "Author", "Publisher", "Subject", "Year"};
@@ -183,12 +211,15 @@ public class GUI {
                     DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>(fields);
                     searchCategory.setModel(model);
                 }
+                // The updates are displayed
                 Main.showRecords(MasterTable);
             }
         });
+        // Search category indicates the column to be searched in a form of a combo box
         searchCategory.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                // The index of the searched column is modified to match the index of the selected item of the combo box
                 Main.searchedColumn = searchCategory.getSelectedIndex()+1;
             }
         });
--- Coursework/src/main/kotlin/Main.java
+++ Coursework/src/main/kotlin/Main.java
@@ -5,12 +5,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
+    // The SQLite connection cursor
     static Connection con;
+    // The SQLite statement
     static Statement stmt;
-    static Integer index;
-    public static boolean isEditing = false;
+    // A list to store all the books
     static List<Book> bookList = new ArrayList<>();
+    // A list to store all the authors
     static List<Author> authorList = new ArrayList<>();
+    // A list to store all the publishers
     static List<Publisher> publisherList = new ArrayList<>();
     static String searchedItem = "";
     static Integer searchedColumn = 1;
@@ -18,6 +21,7 @@ public class Main {
     static String Ticks = null;
 
     public static void main(String[] args) {
+        // The instantiation of the GUI
         JFrame frame = new JFrame("Library Manager: Admin");
         frame.setContentPane(new GUI().GUI_Window);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -25,27 +29,41 @@ public class Main {
         frame.pack();
         frame.setVisible(true);
     }
+    // The function to connect to the database
     public static void Connect(String dbLocation) {
         try {
+            // This is a safe statement to attempt to connect to the SQLite database
             Class.forName("org.sqlite.JDBC");
+            // The string parameter indicates the supposed path to the database
             con = DriverManager.getConnection("jdbc:sqlite:" + dbLocation);
         } catch (SQLException | ClassNotFoundException e) {
+            // A special type of exception specific to the SQLite will be displayed in case an error occurs
+            // Errors a directly taken from SQLite
             throw new RuntimeException(e);
         }
     }
+    // The function to display or refresh the records
     public static void showRecords(JTable MasterTable) {
+        // This is a safe statement, to prevent detrimental damage in case the statements do not match
         try {
+            // The list of the book class is emptied to be filled again with the up-to-date records if not done so
             bookList = new ArrayList<Book>();
+            // The statement has been primed to execute SQL statements
             stmt = con.createStatement();
             String data[][] = {};
 
+            // The data will be retrieved based on the current data type being utilised
             if (dataType == 0){
+                // All records from the books table are retrieved and stored in a result set
                 ResultSet rs = stmt.executeQuery("SELECT *  FROM Books;");
 
+                // The table initially begins as a blank table with the following column labels
                 String column[] = {"id", "title", "author", "publisher", "subject ", "year"};
                 DefaultTableModel tableModel = new DefaultTableModel(data, column);
 
+                // The following loop will keep iterating as long as there are row left on the result set
                 while (rs.next()) {
+                    // Every field of a row is stored within a variable
                     int id = rs.getInt("ID");
                     String title = rs.getString("Title");
                     String author = rs.getString("Author");
@@ -53,8 +71,11 @@ public class Main {
                     String subject = rs.getString("Subject");
                     int year = rs.getInt("Year");
 
+                    // A new row containing all the variables is declared
                     Object[] newRow = new Object[]{id, title, author, publisher, subject, year};
 
+                    // If the searched item query is a non-empty string, only the records corresponding to the search will be displayed
+                    // The search is not case-sensitive
                     if (!searchedItem.equals("")) {
                         if (searchedItem.toLowerCase().equals(newRow[searchedColumn].toString().toLowerCase())) {
                             tableModel.addRow(newRow);
@@ -63,26 +84,33 @@ public class Main {
                         tableModel.addRow(newRow);
                     }
 
+                    // A new book is declared from the variables and added into the list
                     Book newBook = new Book(id, title, author, publisher, subject, year);
                     bookList.add(newBook);
-
-
                 }
+                // The table is updated
                 MasterTable.setModel(tableModel);
             }
             else if (dataType == 1) {
+                // All records from the authors table are retrieved and stored in a result set
                 ResultSet rs = stmt.executeQuery("SELECT * FROM Authors;");
 
+                // The table initially begins as a blank table with the following column labels
                 String column[] = {"id", "firstname", "lastname"};
                 DefaultTableModel tableModel = new DefaultTableModel(data, column);
 
+                // The following loop will keep iterating as long as there are row left on the result set
                 while (rs.next()) {
+                    // Every field of a row is stored within a variable
                     int id = rs.getInt("ID");
                     String firstname = rs.getString("Firstname");
                     String lastname = rs.getString("Lastname");
 
+                    // A new row containing all the variables is declared
                     Object[] newRow = new Object[]{id,firstname,lastname};
 
+                    // If the searched item query is a non-empty string, only the records corresponding to the search will be displayed
+                    // The search is not case-sensitive
                     if (!searchedItem.equals("")) {
                         if (searchedItem.equals(newRow[searchedColumn].toString())) {
                             tableModel.addRow(newRow);
@@ -91,23 +119,32 @@ public class Main {
                         tableModel.addRow(newRow);
                     }
 
+                    // A new author is declared from the variables and added into the list
                     Author newAuthor = new Author(id, firstname, lastname);
                     authorList.add(newAuthor);
                 }
+                // The table is updated
                 MasterTable.setModel(tableModel);
             }
             else if (dataType == 2) {
+                // All records from the publishers table are retrieved and stored in a result set
                 ResultSet rs = stmt.executeQuery("SELECT * FROM Publishers;");
 
+                // The table initially begins as a blank table with the following column labels
                 String column[] = {"id", "name"};
                 DefaultTableModel tableModel = new DefaultTableModel(data, column);
 
+                // The following loop will keep iterating as long as there are row left on the result set
                 while (rs.next()) {
+                    // Every field of a row is stored within a variable
                     int id = rs.getInt("ID");
                     String name = rs.getString("Name");
 
+                    // A new row containing all the variables is declared
                     Object[] newRow = new Object[]{id,name};
 
+                    // If the searched item query is a non-empty string, only the records corresponding to the search will be displayed
+                    // The search is not case-sensitive
                     if (!searchedItem.equals("")) {
                         if (searchedItem.equals(newRow[searchedColumn].toString())) {
                             tableModel.addRow(newRow);
@@ -116,22 +153,26 @@ public class Main {
                         tableModel.addRow(newRow);
                     }
 
+                    // A new author is declared from the variables and added into the list
                     Publisher newPublisher = new Publisher(id, name);
                     publisherList.add(newPublisher);
                 }
+                // The table is updated
                 MasterTable.setModel(tableModel);
             }
         } catch (SQLException e1) {
             e1.printStackTrace();
         }
     }
+    // The function to save the current state of the records in the SQL database
     public static void saveAllRecord() {
         try {
-            // Clearing all rows from table
+            // This safe statement deletes all the records from the database, resets the sequence counter to 0
+            // Once the counter resets, ID numbers begin to be assigned from 1 again
             stmt.executeUpdate("DELETE FROM Books");
-            // Make auto increment restart from 1
             stmt.executeUpdate("UPDATE sqlite_sequence SET seq=0 WHERE name='Books'");
 
+            // Lambda function to insert all the books in their current form (preferably after sorting)
             bookList.forEach(book -> {
                 try {
                     int rs = stmt.executeUpdate("INSERT INTO Books (Title, Author, Publisher, Subject, Year)" +
@@ -149,6 +190,8 @@ public class Main {
         }
     }
 
+    // Sorting algorithm functions to sort the list of books
+    // All of them yield the number of ticks as well
     public static void bubbleSort() {
         BubbleSort.INSTANCE.bubbleSort(bookList);
         Ticks = "BubbleSort - number of ticks " + BubbleSort.INSTANCE.getTicks();
