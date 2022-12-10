4e6ce3c9a5041d1291c1379eb02feabd8265498c
Jaime A Difo Lorenzo <77880449+JaimeDifo@users.noreply.github.com>
2022-12-10 14:11:09 +0000

    Update BubbleSort.kt

--- Coursework/src/main/kotlin/BubbleSort.kt
+++ Coursework/src/main/kotlin/BubbleSort.kt
@@ -2,8 +2,8 @@ object BubbleSort { //Bubble Sort Algo//
     var ticks = 0       //Counts the number of movements in code.//
     fun bubbleSort(bookList: MutableList<Book>): Int {
         ticks = 0
-        for (i in 0 until bookList.size - 1) {  //iteration to loop through the total number of the booklist.//
-            for (j in 0 until bookList.size - i - 1) {
+        for (i in 0 until bookList.size - 1) {  
+            for (j in 0 until bookList.size - i - 1) { //iteration to loop through the total number of the booklist.//
                 ticks ++
                 val book1 = bookList.get(j)  //initialization of variables, use in swapping.//
                 val title1: String = book1.title.lowercase()
