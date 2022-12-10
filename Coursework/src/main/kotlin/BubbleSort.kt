8648125f231e2c5833f4123efcce9fcd5fd0472d
Jaime A Difo Lorenzo <77880449+JaimeDifo@users.noreply.github.com>
2022-12-10 13:58:42 +0000

    Update BubbleSort.kt

--- Coursework/src/main/kotlin/BubbleSort.kt
+++ Coursework/src/main/kotlin/BubbleSort.kt
@@ -1,22 +1,22 @@
-object BubbleSort {
-    var ticks = 0
+object BubbleSort { //Bubble Sort Algo//
+    var ticks = 0       //Counts the number of moving in code.//
     fun bubbleSort(bookList: MutableList<Book>): Int {
         ticks = 0
-        for (i in 0 until bookList.size - 1) {
+        for (i in 0 until bookList.size - 1) {  //iteration to loop through the total number of the booklist.//
             for (j in 0 until bookList.size - i - 1) {
                 ticks ++
-                val book1 = bookList.get(j)
+                val book1 = bookList.get(j)  //initialization of variables, use in swapping.//
                 val title1: String = book1.title.lowercase()
                 val author1 = book1.author.lowercase()
                 val book2 = bookList.get(j + 1)
                 val title2: String = book2.title.lowercase()
                 val author2 = book2.author.lowercase()
 
-                if (author1 > author2) {
+                if (author1 > author2) { //Swapping based on author's name.//
                     val swap: Book = book1
                     bookList[j] = book2
                     bookList[j + 1] = swap
-                } else if (author1 == author2) {
+                } else if (author1 == author2) { //if the authors name are the same swapping based on title of the book.//
                     if (title1 > title2) {
                         val swap: Book = book1
                         bookList[j] = book2
@@ -27,6 +27,6 @@ object BubbleSort {
             }
         }
 
-        return ticks
+        return ticks //Returns the number of ticks to be displayed in the GUI.//
     }
 }
